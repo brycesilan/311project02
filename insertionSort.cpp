ec4d3e49133a80b99b6e20a577c42bdd7ca4423d
Connor Richards <connor.a.t.richards@gmail.com>
2018-09-19 18:11:56 -0700

    added randomness

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -2,32 +2,36 @@
 
 using namespace std;
 
-void getInput(int* &sortingArray, int sizeInput, char caseInput);
-void insertionSort(int* &sortingArray, int sizeInput);
+void getInput(int &sizeInput, char &caseInput);
+void insertionSort(int* sortingArray, int sizeInput);
 void printArray(int* sortingArray, int sizeInput);
-void generateArray(int* &sortingArray, int sizeInput, char caseInput);
+int* generateArray(int sizeInput, char caseInput);
 
 int main() {
   int* sortingArray;
   int sizeInput;
   char caseInput;
 
-  getInput(sortingArray, sizeInput, caseInput);
+  getInput(sizeInput, caseInput);
+
+  sortingArray=generateArray(sizeInput, caseInput);
+  cout << "--- Array has been made, it is: ";
+  printArray(sortingArray, sizeInput);
+
   insertionSort(sortingArray, sizeInput);
+  cout << "--- Array has been sorted, it is: ";
   printArray(sortingArray, sizeInput);
 
   return 0;
 }
 
 
-void getInput(int* &sortingArray, int sizeInput, char caseInput) {
+void getInput(int &sizeInput, char &caseInput) {
   cout << "--- Enter size of array followed by 'b' - best case, 'w' - worst case, or 'a' - average case ---" << endl;
   cin >> sizeInput >> caseInput;
-
-  sortingArray=new int[sizeInput];
 }
 
-void insertionSort(int* &sortingArray, int sizeInput) {
+void insertionSort(int* sortingArray, int sizeInput) {
   for(int idx=1; idx<sizeInput; idx++) {
     int key=sortingArray[idx];
     int idx2=idx-1;
@@ -39,35 +43,38 @@ void insertionSort(int* &sortingArray, int sizeInput) {
   }
 }
 
+int* generateArray(int sizeInput, char caseInput) {
+  int* thisArray=new int[sizeInput];
+
+  switch (caseInput) {
+    case 'b':
+      for(int idx=0; idx<=sizeInput; idx++) {
+        thisArray[idx]=idx+1;
+      }
+      break;
+    case 'w':
+      for(int idx=0; idx<=sizeInput; idx++) {
+        if(idx==0) {
+          thisArray[idx]=sizeInput;
+        }
+        else {
+          thisArray[idx]=(thisArray[idx-1])-1;
+        }
+      }
+      break;
+    case 'a':
+      for(int idx=0; idx<=sizeInput; idx++) {
+        thisArray[idx]=rand()%sizeInput+1;
+      }
+      break;
+  }
+
+  return thisArray;
+}
+
 void printArray(int* sortingArray, int sizeInput) {
   for(int idx=0; idx<sizeInput; idx++) {
     cout << sortingArray[idx] << " ";
   }
   cout << endl;
-}
-
-//others made generate array actually return and array pointer
-void generateArray(int* &sortingArray, int sizeInput, char caseInput) {
-  switch (caseInput) {
-    case 'b':
-      for(int idx=0; idx<sizeInput; idx++) {
-        sortingArray[idx]=idx+1;
-      }
-      break;
-    case 'w':
-      for(int idx=sizeInput; idx>=0; idx--) {
-        if(idx=sizeInput) {
-          sortingArray[idx]=1;
-        }
-        else {
-          sortingArray[idx]=sortingArray[idx+1]+1;
-        }
-      }
-      break;
-    case 'a':
-      break;
-    default:
-      cerr << "Error: Invalid case" << endl;
-      break;
-  }
 }
